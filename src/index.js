fb7a30d1839068dca6b571623fb99091bde1e3ee
Kuponutty <w0184129@nscc.ca>
2023-12-05 18:59:45 -0400

    enhancement 1

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./styles.css";
 
 import App from "./App";
+import Game from './Game';
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
-    <App />
+    <Game />
   </StrictMode>
 );
